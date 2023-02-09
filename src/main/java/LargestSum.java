6b040a14cdcaf1c75bb2a873612b94301f3eba8c
MockTrainee <mock.trainee@aol.com>
2023-02-09 19:41:49 +0000

    Complete Largest Sum Lab

--- src/main/java/LargestSum.java
+++ src/main/java/LargestSum.java
@@ -1,4 +1,4 @@
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class LargestSum {
@@ -10,6 +10,26 @@ public class LargestSum {
      * @return the largest possible sum of separate numbers from nums.
      */
     public int bigSum(List<Integer> nums){
-        return 0;
+        int largest = nums.get(0);
+        int largestIndex = 0;
+
+        int secondLargest = 0;
+
+        // get largest
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) > largest) {
+                largest = nums.get(i);
+                largestIndex = i;
+            }
+        }
+
+        // get second largest
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums.get(i) >= secondLargest && i != largestIndex) {
+                secondLargest = nums.get(i);
+            }
+        }
+
+        return largest + secondLargest;
     }
 }
